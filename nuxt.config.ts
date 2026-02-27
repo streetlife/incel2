21e9b286ae160e1b915ae32f7b1a4b9324ca6bb7
onyiimesi <onyedikachukwu62@gmail.com>
2026-02-27 20:19:41 +0100

    Updated nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -29,7 +29,7 @@ export default defineNuxtConfig({
   },
   runtimeConfig: {
     public: {
-      apiBase: process.env.API_BASE_URL ?? 'http://localhost:8000',
+      apiBase: process.env.NUXT_PUBLIC_API_BASE_URL ?? 'http://localhost:8000',
     },
   },
 })
